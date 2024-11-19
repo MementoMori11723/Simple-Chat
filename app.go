b815f31b9fbf8ca3c5a5eb9d5f35d6e115c6c067
yasasvi <yasasvigumma@gmail.com>
2024-11-19 19:17:05 +0530

    Added a websocket
    this allows us to send data like in a chat app

--- app.go
+++ app.go
@@ -4,7 +4,11 @@ import (
 	"embed"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"sync"
+
+	"golang.org/x/net/websocket"
 )
 
 var (
@@ -12,6 +16,11 @@ var (
 	files  embed.FS
 	layout = "pages/layout.html"
 )
+
+type Server struct {
+	conn map[*websocket.Conn]bool
+	mu   sync.Mutex
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -47,11 +56,60 @@ func errorHandler(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+func NewServer() *Server {
+	return &Server{
+		conn: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("New connection established - connection from: ", ws.RemoteAddr())
+	s.mu.Lock()
+	s.conn[ws] = true
+	s.mu.Unlock()
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Connection closed by client: ", ws.RemoteAddr())
+				s.mu.Lock()
+				delete(s.conn, ws)
+				s.mu.Unlock()
+				break
+			}
+			fmt.Println("Error reading from connection: ", err)
+			continue
+		}
+		msg := buf[:n]
+		s.broadcast(msg)
+	}
+}
+
+func (s *Server) broadcast(b []byte) {
+	s.mu.Lock()
+	for ws := range s.conn {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("Error broadcasting message: ", err)
+			}
+		}(ws)
+	}
+	s.mu.Unlock()
+}
+
 func main() {
-  http.HandleFunc("/", homeHandler)
-  http.HandleFunc("/error", errorHandler)
-  err := http.ListenAndServe(":8080", nil)
-  if err != nil {
-    fmt.Println("Error starting server: ", err)
-  }
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/error", errorHandler)
+	fmt.Println("Server started at :11000")
+	err := http.ListenAndServe(":11000", nil)
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module simple-chat
 
 go 1.23.3
+
+require golang.org/x/net v0.31.0 // indirect
